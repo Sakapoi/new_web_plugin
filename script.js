9dbbbd26619f1d056081e696957a22b786f2a710
Sakapoi <yuhnon@mail.ru>
2023-04-25 06:51:28 +0000

    added Rust function

--- script.js
+++ script.js
@@ -5,50 +5,25 @@ export function getActiveTabUrl(callback) {
   });
 }
 
-export function changeContent(content) {
-  const contentDiv = document.getElementById('content');
+export function openLinkInNewTab(url) {
+  document.getElementById("anime").addEventListener("click", function(event) {
+    event.preventDefault(); // зупинка дії за замовчуванням
+    window.open(url, '_blank');
+  });
+}
 
+export function changeContent(content, id) {
+  const contentDiv = document.getElementById(id);
   contentDiv.innerHTML = content;
 }
 
 export function listProcesses() {
-  console.log("peter");
   chrome.processes.getProcessInfo(function(processes) {
     for (var i = 0; i < processes.length; i++) {
       console.log(processes[i]);
     }
   });
 }
-
-export async function takeScreenshott() {
-  const stream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-  const track = stream.getVideoTracks()[0];
-  const imageCapture = new ImageCapture(track);
-  const blob = await imageCapture.takePhoto();
-  const url = URL.createObjectURL(blob);
-  const img = new Image();
-  img.src = url;
-  document.getElementById("screenshot").appendChild(img);
-}
-
-export const capture = async () => {
-  const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d");
-  const video = document.createElement("video");
-
-  try {
-    const captureStream = await navigator.mediaDevices.getDisplayMedia();
-    video.srcObject = captureStream;
-    context.drawImage(video, 0, 0, window.width, window.height);
-    console.log("1");
-    const frame = canvas.toDataURL("image/png");
-    console.log("2");
-    captureStream.getTracks().forEach(track => track.stop());
-    window.location.href = frame;
-  } catch (err) {
-    console.error("Error: " + err);
-  }
-};
 
 export function screen() {
   chrome.tabs.create({ url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" }, function(tab) {
@@ -57,77 +32,7 @@ export function screen() {
   });
 }
 
-export function takeScreenshotAndDownload() {
-  // Створюємо новий елемент canvas
-  const canvas = document.createElement('canvas');
-  
-  // Встановлюємо розміри canvas такими, які має екран
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  
-  // Отримуємо контекст для малювання на canvas
-  const ctx = canvas.getContext('2d');
-  
-  // Заповнюємо canvas зображенням екрану
-  ctx.drawImage(document.body, 0, 0, canvas.width, canvas.height);
-  
-  // Створюємо посилання на завантаження зображення
-  const link = document.createElement('a');
-  link.download = 'screenshot.png';
-  
-  // Перетворюємо canvas в URL-адрес зображення
-  canvas.toBlob(blob => {
-    const url = URL.createObjectURL(blob);
-    link.href = url;
-    
-    // Клікаємо по посиланню, щоб завантажити зображення
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    
-    // Звільняємо URL-адрес зображення
-    URL.revokeObjectURL(url);
-  }, 'image/png');
-}
-
-export function captureAndCopyToClipboard() {
-  // Створюємо новий об'єкт Image для зберігання скріншоту
-  var img = new Image();
-
-  // Викликаємо функцію для створення скріншоту та передаємо callback, щоб коли скріншот буде готовий, ми могли скопіювати його до буфера обміну
-  chrome.tabs.captureVisibleTab(null, { format: "png" }, function(dataUrl) {
-    img.src = dataUrl;
-
-    // Створюємо новий об'єкт canvas для роботи зі скріншотом
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    // Рендеримо скріншот на canvas
-    var context = canvas.getContext("2d");
-    context.drawImage(img, 0, 0);
-
-    // Отримуємо URL зображення з canvas
-    var imageUrl = canvas.toDataURL("image/png");
-    //console.log(imageUrl);
-    // Копіюємо зображення до буфера обміну
-    var imgData = atob(imageUrl.substring("data:image/png;base64,".length));
-    var buffer = new ArrayBuffer(imgData.length);
-    var view = new Uint8Array(buffer);
-    for (var i = 0; i < imgData.length; i++) {
-      view[i] = imgData.charCodeAt(i);
-    }
-    var blob = new Blob([buffer], { type: "image/png" });
-    var data = [new ClipboardItem({ "image/png": blob })];
-    navigator.clipboard.write(data).then(function() {
-      console.log("Скріншот був скопійований до буфера обміну!");
-    }, function() {
-      console.error("Помилка копіювання скріншоту до буфера обміну!");
-    });
-  });
-}
-
-export function qwe() {
+export function take_screen_from_js() {
   // Request media
   navigator.mediaDevices.getDisplayMedia().then(stream => 
   {
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-use wasm_bindgen::Clamped;
 use wasm_bindgen::prelude::*;
 use web_sys::*;
-use js_sys::*;
 
 macro_rules! console_log {
   ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
@@ -16,14 +14,11 @@ extern "C" {
 #[wasm_bindgen(module = "/script.js")]
 extern "C" {
     fn getActiveTabUrl(callback: &Closure<dyn FnMut(Vec<JsValue>)>);
-    fn changeContent(content: JsValue);
+    fn changeContent(content: JsValue, id: String);
     fn listProcesses();
     fn screen();
-    fn takeScreenshott();
-    fn captureAndCopyToClipboard();
-    fn capture();
-    fn takeScreenshotAndDownload();
-    fn qwe();
+    fn take_screen_from_js();
+    fn openLinkInNewTab(url: String);
 }
 
 #[wasm_bindgen]
@@ -36,15 +31,15 @@ pub fn my_plugin_function() {
         console_log!("{:?}", url_str);
         let pretty_html_list = format!(
             r#"<ul class="styled-list">
-      {}
-      </ul>"#,
+            {}
+            </ul>"#,
             url_str
                 .into_iter()
                 .map(|url| format!("<li>{url}</li>"))
                 .collect::<Vec<_>>()
                 .join("")
         );
-        changeContent(pretty_html_list.into());
+        changeContent(pretty_html_list.into(), "content".to_string());
     });
     getActiveTabUrl(&closure);
 
@@ -52,93 +47,40 @@ pub fn my_plugin_function() {
 }
 
 #[wasm_bindgen]
-pub fn take_screenshot() -> Result<JsValue, JsValue> {
-    console_log!("peter");
-    let window = web_sys::window().unwrap();
-    console_log!("peter");
-    let document = window.document().unwrap();
-    console_log!("peter");
-    let canvas = document.create_element("canvas")?.dyn_into::<HtmlCanvasElement>()?;
-    let context = canvas.get_context("2d")?.unwrap().dyn_into::<CanvasRenderingContext2d>()?;
-    console_log!("sanya");
-    canvas.set_width(window.inner_width()?.as_f64().unwrap() as u32);
-    canvas.set_height(window.inner_height()?.as_f64().unwrap() as u32);
-    console_log!("sanya {:?}", window);
-    console_log!("sanya {:?}", context);
-    let peter = vec![100; 4*100*100];
-    console_log!("{}", peter.len());
-    let data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&peter), 100, 100);
-    console_log!("sanya {:?}", data);
-    context.put_image_data(&data.unwrap(), 100.0, 100.0)?;
-    console_log!("sanya");
-    let (width, height) = (
-        window.inner_width()?.as_f64().unwrap() as u32,
-        window.inner_height()?.as_f64().unwrap() as u32,
-    );
-    canvas.set_width(width);
-    canvas.set_height(height);
-    console_log!("sanya1");
-    let data_url_promise = js_sys::Promise::new(&mut |resolve, _reject| {
-        let f = Closure::wrap(Box::new(move |data_url: String| {
-            resolve.call1(&JsValue::NULL, &JsValue::from_str(&data_url)).unwrap();
-        }) as Box<dyn FnMut(String)>);
-        window.request_animation_frame(f.as_ref().unchecked_ref()).unwrap();
-        let _ = f.forget();
-    });
-    console_log!("sanya2 {:?}", data_url_promise);
-    let data_url = data_url_promise.as_string().unwrap();
-    console_log!("sanya {}", data_url);
-    let img = web_sys::HtmlImageElement::new()?;
-    img.set_src(&data_url);
-    
-    context.draw_image_with_html_image_element(&img, width as f64, height as f64)?;
-    console_log!("max875");
-    Ok(JsValue::from_str(&canvas.to_data_url()?))
-}
-
-#[wasm_bindgen]
-pub fn take_screen_from_js() {
+pub fn take_screen_from_rust() {
     let navigator = web_sys::window().unwrap().navigator();
     let media_devices = navigator.media_devices().unwrap();
-   
-    let promise: Promise = media_devices.get_display_media().unwrap().into();
-    let closure = Closure::once_into_js(move |stream: MediaStream| {
-        let track: MediaStreamTrack = stream.get_video_tracks().get(0).into();
-        let js_track = JsValue::from(track);
-        let capture = js_sys::Reflect::get(&js_track, &JsValue::from_str("ImageCapture"))
-        .unwrap()
-        .dyn_into::<web_sys::ImageCapture>()
-        .unwrap();
-        let grab_promise = capture.grab_frame();
-        
-        let canvas: HtmlCanvasElement = window().unwrap().document().unwrap().get_element_by_id("myCanvas").unwrap().dyn_into().unwrap();
 
-        let f = Closure::wrap(Box::new(|bitmap: js_sys::Object| {
-            let bitmap: web_sys::ImageData = bitmap.unchecked_into();
+    let closure = Closure::new(move |stream: JsValue| {
+        let track: MediaStreamTrack = stream.dyn_into::<web_sys::MediaStream>().unwrap().
+        get_video_tracks().get(0).into();
+        let capture = ImageCapture::new(&track).unwrap();
+
+        let f = Closure::new(move |bitmap: JsValue| {
+            track.stop();
+            let canvas: HtmlCanvasElement = window().unwrap()
+            .document().unwrap().get_element_by_id("myCanvas").unwrap().dyn_into().unwrap();
+            let bitmap: web_sys::HtmlImageElement = bitmap.unchecked_into();
             canvas.set_width(bitmap.width() as u32);
             canvas.set_height(bitmap.height() as u32);
             canvas.get_context("2d").unwrap().unwrap()
             .dyn_into::<CanvasRenderingContext2d>().unwrap()
-            .draw_image_with_html_image_element(&bitmap.dyn_into::<HtmlImageElement>().unwrap(), 0.0, 0.0).unwrap();
+            .draw_image_with_html_image_element(&bitmap, 0.0, 0.0)
+            .unwrap();
+    
+            let screenshot_url = canvas.to_data_url().unwrap();
 
-            let url_promise = canvas.to_data_url().unwrap();
-            console_log!("{}", url_promise);
-            let callback = Function::new_no_args("");
-            let f = Closure::wrap(Box::new(move |url: JsValue| {
-                callback.call1(&JsValue::NULL, &url).unwrap();
-            }) as Box<dyn FnMut(JsValue)>);
-            //url_promise.then(&f.as_ref().unchecked_ref()).unwrap();
-            f.forget();
-        }) as Box<dyn FnMut(js_sys::Object)>);
-        
-        grab_promise.then(&f.as_ref().unchecked_ref()).unwrap();
+            console_log!("{}", screenshot_url);
+            //open_new_tab(screenshot_url);
+            changeContent(screenshot_url.into(), "content".to_string());
+        });
+
+        let _ = capture.grab_frame().then(&f);
         f.forget();
-
-        let _ = track.stop();
     });
-
-    // let _ = promise.then(&closure);
-    // closure.forget();
+   
+    let _ = media_devices.get_display_media().unwrap().then(&closure);
+    closure.forget();
 }
 
 #[wasm_bindgen]
@@ -147,7 +89,14 @@ pub fn open_new_tab(url: String) {
 
     window
     .open_with_url(&url)
-    .expect("Не вдалося відкрити вкладку");
+    .expect("Unable to open a new tab");
+}
+
+#[wasm_bindgen]
+pub fn get_text_by_id(id: &str) -> String {
+    let elemet = window().unwrap()
+    .document().unwrap().get_element_by_id(id).unwrap();
+    elemet.text_content().unwrap()
 }
 
 #[wasm_bindgen]
@@ -157,15 +106,14 @@ pub fn print_processes() {
 
 #[wasm_bindgen]
 pub fn take_screen() {
-    console_log!("peter");
-    //qwe();
-    take_screen_from_js();
-    //console_log!("{:?}", take_screenshot().unwrap());
-    // let url = take_screenshot().unwrap().as_string().unwrap();
-    // open_new_tab(url);
+    //take_screen_from_js();
+    take_screen_from_rust();
+    
 }
 
 #[wasm_bindgen]
 pub fn dont_press() {
-    screen();
+    let url = get_text_by_id("content");
+    //open_new_tab(url);
+    openLinkInNewTab(url);
 }
